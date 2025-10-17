4363b5cce804844b3101c9916f4b54edee7d2bc7
salma <salma.elsaid04@eng-st.cu.edu.eg>
2025-10-17 16:34:26 +0300

    Add files via upload

---
+++ DFT/cons.tcl
@@ -0,0 +1,202 @@
+
+# Constraints
+# ----------------------------------------------------------------------------
+#
+# 1. Master Clock Definitions
+#
+# 2. Generated Clock Definitions
+#
+# 3. Clock Uncertainties
+#
+# 4. Clock Latencies 
+#
+# 5. Clock Relationships
+#
+# 6. set input/output delay on ports
+#
+# 7. Driving cells
+#
+# 8. Output load
+
+####################################################################################
+           #########################################################
+                  #### Section 1 : Clock Definition ####
+           #########################################################
+#################################################################################### 
+# 1. Master Clock Definitions 
+# 2. Generated Clock Definitions
+# 3. Clock Latencies
+# 4. Clock Uncertainties
+# 4. Clock Transitions
+####################################################################################
+ 
+set CLK_NAME REF_CLK
+set CLK_PER 20         
+set CLK_SETUP_SKEW 0.2
+set CLK_HOLD_SKEW 0.1
+set CLK_LAT 0
+set CLK_RISE 0.05
+set CLK_FALL 0.05
+
+set CLK_NAME_2 UART_CLK
+set CLK_PER_2 271.267      
+set RX_PERIOD $CLK_PER_2
+set TX_PERIOD [expr $CLK_PER_2 * 32]
+set ALU_PERIOD $CLK_PER
+
+## Master Clocks
+create_clock -name $CLK_NAME -period $CLK_PER -waveform "0 [expr $CLK_PER/2.0]" [get_ports REF_CLK]
+set_clock_uncertainty -setup $CLK_SETUP_SKEW [get_clocks $CLK_NAME]
+set_clock_uncertainty -hold $CLK_HOLD_SKEW  [get_clocks $CLK_NAME]
+set_clock_transition -rise $CLK_RISE  [get_clocks $CLK_NAME]
+set_clock_transition -fall $CLK_FALL  [get_clocks $CLK_NAME]
+set_clock_latency $CLK_LAT [get_clocks $CLK_NAME]
+set_dont_touch_network "$CLK_NAME"
+
+
+create_clock -name $CLK_NAME_2 -period $CLK_PER_2 -waveform "0 [expr $CLK_PER_2/2.0]" [get_ports UART_CLK]
+set_clock_uncertainty -setup $CLK_SETUP_SKEW [get_clocks $CLK_NAME_2]
+set_clock_uncertainty -hold $CLK_HOLD_SKEW  [get_clocks $CLK_NAME_2]
+set_clock_transition -rise $CLK_RISE  [get_clocks $CLK_NAME_2]
+set_clock_transition -fall $CLK_FALL  [get_clocks $CLK_NAME_2]
+set_clock_latency $CLK_LAT [get_clocks $CLK_NAME_2]
+set_dont_touch_network "$CLK_NAME_2"
+
+## Generated clocks
+
+create_generated_clock -master_clock $CLK_NAME -source [get_ports REF_CLK] \
+                       -name "ALU_Gated_CLK" [get_port alu_inst/CLK] \
+                       -divide_by 1
+set_dont_touch_network "ALU_Gated_CLK"
+
+create_generated_clock -master_clock $CLK_NAME_2 -source [get_ports UART_CLK] \
+                       -name "UART_RX_CLK" [get_port clk_div2_inst/div_clk] \
+                       -divide_by 1
+set_dont_touch_network "UART_RX_CLK"
+
+create_generated_clock -master_clock $CLK_NAME_2 -source [get_ports UART_CLK] \
+                       -name "UART_TX_CLK" [get_port clk_div_inst/div_clk] \
+                       -divide_by 32
+set_dont_touch_network "UART_TX_CLK"
+set_clock_uncertainty -setup $CLK_SETUP_SKEW [get_clocks "ALU_Gated_CLK"]
+set_clock_uncertainty -hold $CLK_HOLD_SKEW  [get_clocks "ALU_Gated_CLK"]
+
+set_clock_uncertainty -setup $CLK_SETUP_SKEW [get_clocks "UART_RX_CLK"]
+set_clock_uncertainty -hold $CLK_HOLD_SKEW  [get_clocks "UART_RX_CLK"]
+
+set_clock_uncertainty -setup $CLK_SETUP_SKEW [get_clocks "UART_TX_CLK"]
+set_clock_uncertainty -hold $CLK_HOLD_SKEW  [get_clocks "UART_TX_CLK"]
+		 
+####### scan clk #########
+set CLK_NAME_scan CLK_scan
+set CLK_PER_scan 100
+set CLK_SETUP_SKEW_scan 0.2
+set CLK_HOLD_SKEW_scan 0.1
+set CLK_RISE_scan 0.1
+set CLK_FALL_scan 0.1
+
+create_clock -name $CLK_NAME_scan -period $CLK_PER_scan -waveform "0 [expr $CLK_PER_scan/2]" [get_ports scan_clk]
+set_clock_uncertainty -setup $CLK_SETUP_SKEW_scan  [get_clocks $CLK_NAME_scan]
+set_clock_uncertainty -hold $CLK_HOLD_SKEW_scan  [get_clocks $CLK_NAME_scan]
+set_clock_transition -rise $CLK_RISE_scan  [get_clocks $CLK_NAME_scan]
+set_clock_transition -fall $CLK_FALL_scan  [get_clocks $CLK_NAME_scan]
+set_clock_latency 0 [get_clocks $CLK_NAME_scan] 
+set_dont_touch_network "$CLK_NAME_scan"	
+# set_dont_touch_network [get_ports scan_rst]	   
+					  
+
+####################################################################################
+           #########################################################
+                  #### Section 2 : Clocks Relationships ####
+           #########################################################
+####################################################################################
+
+set_clock_groups -asynchronous -group [get_clocks "$CLK_NAME ALU_Gated_CLK"]  \
+                               -group [get_clocks "$CLK_NAME_2 UART_TX_CLK UART_RX_CLK"] \
+                               -group [get_clocks "$CLK_NAME_scan"] 
+
+####################################################################################
+           #########################################################
+             #### Section 3 : set input/output delay on ports ####
+           #########################################################
+####################################################################################
+
+set in_delay  [expr 0.2*$CLK_PER_2]
+set out_delay [expr 0.2*$CLK_PER_2]
+
+set in_delay_scan  [expr 0.2*$CLK_PER_scan]
+set out_delay_scan [expr 0.2*$CLK_PER_scan]
+
+#Constrain Input Paths
+set_input_delay $in_delay -clock $CLK_NAME_2 [get_port UART_RX_IN]
+
+set_input_delay $in_delay_scan -clock $CLK_NAME_scan [get_port SI]
+set_input_delay $in_delay_scan -clock $CLK_NAME_scan [get_port SE]
+set_input_delay $in_delay_scan -clock $CLK_NAME_scan [get_port test_mode]
+
+
+#Constrain Output Paths
+set_output_delay $out_delay -clock  UART_TX_CLK [get_port UART_TX_O]
+set_output_delay $out_delay -clock  $CLK_NAME_2 [get_port framing_error]
+set_output_delay $out_delay -clock  $CLK_NAME_2 [get_port parity_error]
+
+set_output_delay $out_delay_scan -clock $CLK_NAME_scan [get_port SO]
+
+
+
+####################################################################################
+           #########################################################
+                  #### Section 4 : Driving cells ####
+           #########################################################
+####################################################################################
+set_fix_multiple_port_nets -all -buffer_constants -feedthroughs
+
+
+set_driving_cell -library scmetro_tsmc_cl013g_rvt_ss_1p08v_125c -lib_cell BUFX2M -pin Y [get_port UART_RX_IN]
+
+set_driving_cell -library scmetro_tsmc_cl013g_rvt_ss_1p08v_125c -lib_cell BUFX2M -pin Y [get_port test_mode]
+set_driving_cell -library scmetro_tsmc_cl013g_rvt_ss_1p08v_125c -lib_cell BUFX2M -pin Y [get_port SI]
+set_driving_cell -library scmetro_tsmc_cl013g_rvt_ss_1p08v_125c -lib_cell BUFX2M -pin Y [get_port SE]
+
+
+
+####################################################################################
+           #########################################################
+                  #### Section 5 : Output load ####
+           #########################################################
+####################################################################################
+
+set_load 0.1 [get_port UART_TX_O]
+set_load 0.1 [get_port parity_error]
+set_load 0.1 [get_port framing_error]
+set_load 0.1 [get_port SO]
+
+
+####################################################################################
+           #########################################################
+                 #### Section 6 : Operating Condition ####
+           #########################################################
+####################################################################################
+
+# Define the Worst Library for Max(#setup) analysis
+# Define the Best Library for Min(hold) analysis
+
+set_operating_conditions -min_library "scmetro_tsmc_cl013g_rvt_ff_1p32v_m40c" -min "scmetro_tsmc_cl013g_rvt_ff_1p32v_m40c" -max_library "scmetro_tsmc_cl013g_rvt_ss_1p08v_125c" -max "scmetro_tsmc_cl013g_rvt_ss_1p08v_125c"
+
+####################################################################################
+           #########################################################
+                  #### Section 7 : wireload Model ####
+           #########################################################
+####################################################################################
+# set_wire_load_model -name tsmc13_wl30 -library scmetro_tsmc_cl013g_rvt_ss_1p08v_125c
+
+
+####################################################################################
+           #########################################################
+                  #### Section 8 : multicycle path ####
+           #########################################################
+####################################################################################
+
+set_case_analysis 1 [get_ports test_mode]
+
+
---
+++ DFT/dft_script.tcl
@@ -0,0 +1,148 @@
+
+########################### Define Top Module ############################
+                                                   
+set top_module SYS_TOP
+
+##################### Define Working Library Directory ######################
+                                                   
+define_design_lib work -path ./work
+
+############################# Formality Setup File ##########################
+                                                   
+set_svf $top_module.svf
+
+
+################## Design Compiler Library Files #setup ######################
+
+lappend search_path  /home/IC/Projects/SYSTEM/std_cells
+lappend search_path  /home/IC/Projects/SYSTEM/rtl
+
+set SSLIB "scmetro_tsmc_cl013g_rvt_ss_1p08v_125c.db"
+set TTLIB "scmetro_tsmc_cl013g_rvt_tt_1p2v_25c.db"
+set FFLIB "scmetro_tsmc_cl013g_rvt_ff_1p32v_m40c.db"
+
+## Standard Cell libraries 
+set target_library [list $SSLIB $TTLIB $FFLIB]
+
+## Standard Cell & Hard Macros libraries 
+set link_library [list * $SSLIB $TTLIB $FFLIB]  
+
+#echo "###############################################"
+#echo "############# Reading RTL Files  ##############"
+#echo "###############################################"
+
+#SYSTEM Files
+analyze -define {DATA_WIDTH=8 RF_ADDR=4 NUM_OF_CHAINS = 3} -format verilog [glob /home/IC/Projects/SYSTEM/rtl/*.v]
+
+elaborate  $top_module
+
+###################### Defining toplevel ###################################
+
+current_design $top_module
+
+
+#################### Liniking All The Design Parts #########################
+puts "###############################################"
+puts "######## Liniking All The Design Parts ########"
+puts "###############################################"
+
+link 
+
+#################### Liniking All The Design Parts #########################
+puts "###############################################"
+puts "######## checking design consistency ##########"
+puts "###############################################"
+
+check_design >> check_design.rpt
+
+#################### Define Design Constraints #########################
+puts "###############################################"
+puts "############ Design Constraints #### ##########"
+puts "###############################################"
+
+source -echo ./cons.tcl
+
+
+
+#################### Archirecture Scan Chains #########################
+puts "###############################################"
+puts "############ Configure scan chains ############"
+puts "###############################################"
+
+set_scan_configuration -clock_mixing no_mix  -style multiplexed_flip_flop -replace true -max_length 100  
+
+###################### Mapping and optimization ########################
+puts "###############################################"
+puts "########## Mapping & Optimization #############"
+puts "###############################################"
+
+compile -scan
+
+########################## Define DFT Signals ##########################
+
+set_dft_signal -port [get_ports scan_clk] -type ScanClock -view existing_dft -timing {30 60}
+set_dft_signal -port [get_ports scan_rst] -type Reset -view existing_dft -active_state 0
+set_dft_signal -port [get_ports test_mode] -type Constant -view existing_dft -active_state 1
+set_dft_signal -port [get_ports test_mode] -type TestMode -view spec -active_state 1
+set_dft_signal -port [get_ports SE] -type ScanEnable -view spec -active_state 1 -usage scan
+
+set_dft_signal -port [get_ports SI] -type ScanDataIn -view spec 
+set_dft_signal -port [get_ports SO] -type ScanDataOut -view spec 
+
+
+################################################################### 
+# Setting Test Timing Variables
+################################################################### 
+
+# Preclock Measure Protocol (default protocol)
+set test_default_period 100
+set test_default_delay 0
+set test_default_bidir_delay 0
+set test_default_strobe 20
+set test_default_strobe_width 0
+############################# Create Test Protocol #####################
+                      
+create_test_protocol 
+
+###################### Pre-DFT Design Rule Checking ####################
+
+dft_drc -verbose
+
+############################# Preview DFT ##############################
+
+preview_dft -show scan_summary
+
+############################# Insert DFT ###############################
+
+insert_dft
+
+######################## Optimize Logic post DFT #######################
+
+compile -scan -incremental
+
+
+###################### Design Rule Checking post DFT ###################
+
+dft_drc -verbose -coverage_estimate
+
+########### close formality file ##############
+
+ set_svf -off
+
+#############################################################################
+# Write out Design after initial compile
+#############################################################################
+write_file -format verilog -hierarchy -output netlists/$top_module.ddc
+write_file -format verilog -hierarchy -output netlists/$top_module.v
+write_sdf  sdf/$top_module.sdf
+write_sdc  -nosplit sdc/$top_module.sdc
+
+################# reporting #######################
+report_area -hierarchy > reports/area.rpt
+report_power -hierarchy > reports/power.rpt
+report_timing -delay_type min > reports/hold.rpt
+report_timing -delay_type max > reports/setup.rpt
+report_clock -attributes > reports/clocks.rpt
+
+
+#gui_start
